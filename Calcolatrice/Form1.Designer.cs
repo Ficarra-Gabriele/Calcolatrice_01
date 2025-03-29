aaade868855c39778e82bdccdb19166df4c14117
ficarra_gabriele <ficarra.gabriele@franchettisalviani.net>
2025-03-29 09:09:53 +0100

    1

---
+++ Calcolatrice/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Calcolatrice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.N1 = new System.Windows.Forms.Button();
+            this.N2 = new System.Windows.Forms.Button();
+            this.N3 = new System.Windows.Forms.Button();
+            this.N9 = new System.Windows.Forms.Button();
+            this.N8 = new System.Windows.Forms.Button();
+            this.N7 = new System.Windows.Forms.Button();
+            this.N6 = new System.Windows.Forms.Button();
+            this.N5 = new System.Windows.Forms.Button();
+            this.N4 = new System.Windows.Forms.Button();
+            this.N0 = new System.Windows.Forms.Button();
+            this.Cancella = new System.Windows.Forms.Button();
+            this.Dividi = new System.Windows.Forms.Button();
+            this.Moltiplica = new System.Windows.Forms.Button();
+            this.Sottrai = new System.Windows.Forms.Button();
+            this.Somma = new System.Windows.Forms.Button();
+            this.Uguale = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Risultato = new System.Windows.Forms.Label();
+            this.N12 = new System.Windows.Forms.Label();
+            this.N11 = new System.Windows.Forms.Label();
+            this.OP01 = new System.Windows.Forms.Label();
+            this.Operazione = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // N1
+            // 
+            this.N1.Location = new System.Drawing.Point(265, 323);
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(75, 23);
+            this.N1.TabIndex = 0;
+            this.N1.Text = "1";
+            this.N1.Click += new System.EventHandler(this.N1_Click);
+            // 
+            // N2
+            // 
+            this.N2.Location = new System.Drawing.Point(346, 323);
+            this.N2.Name = "N2";
+            this.N2.Size = new System.Drawing.Size(75, 23);
+            this.N2.TabIndex = 1;
+            this.N2.Text = "2";
+            this.N2.Click += new System.EventHandler(this.N2_Click);
+            // 
+            // N3
+            // 
+            this.N3.Location = new System.Drawing.Point(427, 323);
+            this.N3.Name = "N3";
+            this.N3.Size = new System.Drawing.Size(75, 23);
+            this.N3.TabIndex = 2;
+            this.N3.Text = "3";
+            this.N3.Click += new System.EventHandler(this.N3_Click);
+            // 
+            // N9
+            // 
+            this.N9.Location = new System.Drawing.Point(427, 265);
+            this.N9.Name = "N9";
+            this.N9.Size = new System.Drawing.Size(75, 23);
+            this.N9.TabIndex = 3;
+            this.N9.Text = "9";
+            this.N9.Click += new System.EventHandler(this.N9_Click);
+            // 
+            // N8
+            // 
+            this.N8.Location = new System.Drawing.Point(346, 265);
+            this.N8.Name = "N8";
+            this.N8.Size = new System.Drawing.Size(75, 23);
+            this.N8.TabIndex = 4;
+            this.N8.Text = "8";
+            this.N8.Click += new System.EventHandler(this.N8_Click);
+            // 
+            // N7
+            // 
+            this.N7.Location = new System.Drawing.Point(265, 265);
+            this.N7.Name = "N7";
+            this.N7.Size = new System.Drawing.Size(75, 23);
+            this.N7.TabIndex = 5;
+            this.N7.Text = "7";
+            this.N7.Click += new System.EventHandler(this.N7_Click);
+            // 
+            // N6
+            // 
+            this.N6.Location = new System.Drawing.Point(427, 294);
+            this.N6.Name = "N6";
+            this.N6.Size = new System.Drawing.Size(75, 23);
+            this.N6.TabIndex = 6;
+            this.N6.Text = "6";
+            this.N6.Click += new System.EventHandler(this.N6_Click);
+            // 
+            // N5
+            // 
+            this.N5.Location = new System.Drawing.Point(346, 294);
+            this.N5.Name = "N5";
+            this.N5.Size = new System.Drawing.Size(75, 23);
+            this.N5.TabIndex = 7;
+            this.N5.Text = "5";
+            this.N5.Click += new System.EventHandler(this.N5_Click);
+            // 
+            // N4
+            // 
+            this.N4.Location = new System.Drawing.Point(265, 294);
+            this.N4.Name = "N4";
+            this.N4.Size = new System.Drawing.Size(75, 23);
+            this.N4.TabIndex = 8;
+            this.N4.Text = "4";
+            this.N4.Click += new System.EventHandler(this.N4_Click);
+            // 
+            // N0
+            // 
+            this.N0.Location = new System.Drawing.Point(346, 352);
+            this.N0.Name = "N0";
+            this.N0.Size = new System.Drawing.Size(75, 23);
+            this.N0.TabIndex = 9;
+            this.N0.Text = "0";
+            this.N0.Click += new System.EventHandler(this.N0_Click);
+            // 
+            // Cancella
+            // 
+            this.Cancella.Location = new System.Drawing.Point(508, 207);
+            this.Cancella.Name = "Cancella";
+            this.Cancella.Size = new System.Drawing.Size(75, 23);
+            this.Cancella.TabIndex = 10;
+            this.Cancella.Text = "Cancella";
+            // 
+            // Dividi
+            // 
+            this.Dividi.Location = new System.Drawing.Point(508, 236);
+            this.Dividi.Name = "Dividi";
+            this.Dividi.Size = new System.Drawing.Size(75, 23);
+            this.Dividi.TabIndex = 11;
+            this.Dividi.Text = "/";
+            this.Dividi.Click += new System.EventHandler(this.Dividi_Click);
+            // 
+            // Moltiplica
+            // 
+            this.Moltiplica.Location = new System.Drawing.Point(508, 265);
+            this.Moltiplica.Name = "Moltiplica";
+            this.Moltiplica.Size = new System.Drawing.Size(75, 23);
+            this.Moltiplica.TabIndex = 12;
+            this.Moltiplica.Text = "*";
+            this.Moltiplica.Click += new System.EventHandler(this.Moltiplica_Click);
+            // 
+            // Sottrai
+            // 
+            this.Sottrai.Location = new System.Drawing.Point(508, 294);
+            this.Sottrai.Name = "Sottrai";
+            this.Sottrai.Size = new System.Drawing.Size(75, 23);
+            this.Sottrai.TabIndex = 13;
+            this.Sottrai.Text = "-";
+            this.Sottrai.Click += new System.EventHandler(this.Sottrai_Click);
+            // 
+            // Somma
+            // 
+            this.Somma.Location = new System.Drawing.Point(508, 323);
+            this.Somma.Name = "Somma";
+            this.Somma.Size = new System.Drawing.Size(75, 23);
+            this.Somma.TabIndex = 14;
+            this.Somma.Text = "+";
+            this.Somma.Click += new System.EventHandler(this.Somma_Click);
+            // 
+            // Uguale
+            // 
+            this.Uguale.Location = new System.Drawing.Point(508, 352);
+            this.Uguale.Name = "Uguale";
+            this.Uguale.Size = new System.Drawing.Size(75, 23);
+            this.Uguale.TabIndex = 15;
+            this.Uguale.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // Risultato
+            // 
+            this.Risultato.AutoSize = true;
+            this.Risultato.Location = new System.Drawing.Point(452, 207);
+            this.Risultato.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Risultato.Name = "Risultato";
+            this.Risultato.Size = new System.Drawing.Size(50, 50);
+            this.Risultato.TabIndex = 16;
+            this.Risultato.Text = "Risultato";
+            // 
+            // N12
+            // 
+            this.N12.AutoSize = true;
+            this.N12.Location = new System.Drawing.Point(340, 207);
+            this.N12.MinimumSize = new System.Drawing.Size(50, 50);
+            this.N12.Name = "N12";
+            this.N12.Size = new System.Drawing.Size(50, 50);
+            this.N12.TabIndex = 17;
+            this.N12.Text = "N2";
+            // 
+            // N11
+            // 
+            this.N11.AutoSize = true;
+            this.N11.Location = new System.Drawing.Point(228, 207);
+            this.N11.MinimumSize = new System.Drawing.Size(50, 50);
+            this.N11.Name = "N11";
+            this.N11.Size = new System.Drawing.Size(50, 50);
+            this.N11.TabIndex = 18;
+            this.N11.Text = "N1";
+            // 
+            // OP01
+            // 
+            this.OP01.AutoSize = true;
+            this.OP01.Location = new System.Drawing.Point(396, 207);
+            this.OP01.MinimumSize = new System.Drawing.Size(50, 50);
+            this.OP01.Name = "OP01";
+            this.OP01.Size = new System.Drawing.Size(50, 50);
+            this.OP01.TabIndex = 19;
+            this.OP01.Text = "=";
+            // 
+            // Operazione
+            // 
+            this.Operazione.AutoSize = true;
+            this.Operazione.Location = new System.Drawing.Point(284, 207);
+            this.Operazione.MinimumSize = new System.Drawing.Size(50, 50);
+            this.Operazione.Name = "Operazione";
+            this.Operazione.Size = new System.Drawing.Size(50, 50);
+            this.Operazione.TabIndex = 20;
+            this.Operazione.Text = "OP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 485);
+            this.Controls.Add(this.Operazione);
+            this.Controls.Add(this.OP01);
+            this.Controls.Add(this.N11);
+            this.Controls.Add(this.N12);
+            this.Controls.Add(this.Risultato);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Uguale);
+            this.Controls.Add(this.Somma);
+            this.Controls.Add(this.Sottrai);
+            this.Controls.Add(this.Moltiplica);
+            this.Controls.Add(this.Dividi);
+            this.Controls.Add(this.Cancella);
+            this.Controls.Add(this.N0);
+            this.Controls.Add(this.N4);
+            this.Controls.Add(this.N5);
+            this.Controls.Add(this.N6);
+            this.Controls.Add(this.N7);
+            this.Controls.Add(this.N8);
+            this.Controls.Add(this.N9);
+            this.Controls.Add(this.N3);
+            this.Controls.Add(this.N2);
+            this.Controls.Add(this.N1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button N2;
+        public System.Windows.Forms.Button N3;
+        public System.Windows.Forms.Button N9;
+        public System.Windows.Forms.Button N8;
+        public System.Windows.Forms.Button N7;
+        public System.Windows.Forms.Button N6;
+        public System.Windows.Forms.Button N5;
+        public System.Windows.Forms.Button N4;
+        public System.Windows.Forms.Button N1;
+        public System.Windows.Forms.Button N0;
+        public System.Windows.Forms.Button Cancella;
+        public System.Windows.Forms.Button Dividi;
+        public System.Windows.Forms.Button Moltiplica;
+        public System.Windows.Forms.Button Sottrai;
+        public System.Windows.Forms.Button Somma;
+        public System.Windows.Forms.Button Uguale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Risultato;
+        private System.Windows.Forms.Label N12;
+        private System.Windows.Forms.Label N11;
+        private System.Windows.Forms.Label OP01;
+        private System.Windows.Forms.Label Operazione;
+    }
+}
+
---
+++ Calcolatrice/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Calcolatrice
+{
+    public partial class Form1 : Form
+    {
+        int N01 = 001001; // Ho scelto un numero che un utente normale non sceglierebbe mai, in modo tale che se un utente seleziona 0 come primo numero non fa confusione il computer
+        int N02 = 001001;
+        int N03 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void N1_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 1;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 1;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N2_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 2;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 2;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N3_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 3;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 3;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N4_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 4;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 4;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N5_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 5;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 5;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N6_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 6;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 6;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N7_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 7;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 7;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N8_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 8;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 8;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N9_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 9;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 9;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void N0_Click(object sender, EventArgs e)
+        {
+            if (N01 == 001001)
+            {
+                N01 = 0;
+                N11.Text = N01.ToString();
+            }
+            else
+            {
+                N02 = 0;
+                N12.Text = N02.ToString();
+            }
+        }
+
+        private void Somma_Click(object sender, EventArgs e)
+        {
+            if (N01 != 001001 && N02 != 001001)
+            {
+                N03 = N01 + N02;
+            }
+        }
+
+        private void Sottrai_Click(object sender, EventArgs e)
+        {
+            if (N01 != 001001 && N02 != 001001)
+            {
+                N03 = N02 - N01;
+            }
+        }
+
+        private void Moltiplica_Click(object sender, EventArgs e)
+        {
+            if (N01 != 001001 && N02 != 001001)
+            {
+                N03 = N01 * N02;
+            }
+        }
+
+        private void Dividi_Click(object sender, EventArgs e)
+        {
+            if (N01 != 001001 && N02 != 001001)
+            {
+                N03 = N01 /  N02;
+            }
+        }
+        private void Uguale_Click(object sender, EventArgs e)
+        {
+            Risultato.Text = N03.ToString();
+        }
+    }
+}
